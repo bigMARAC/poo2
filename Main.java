546405f6cc8b0422fd3d33d21906e6b85f15b033
bigMARAC <marcos.length@gmail.com>
2024-03-26 11:03:32 -0300

    creates a date comparer

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
    public static void main(String[] args) {
-      // code
+      Scanner input = new Scanner(System.in);
+      
+      System.out.print("Digite a primeira data (dd/MM/yyyy): ");
+      String date01 = input.next();
+      
+      System.out.print("Digite a segunda data (dd/MM/yyyy): ");
+      String date02 = input.next();
+      try {
+         System.out.println(DateController.compare(date01, date02));
+      } catch (Exception e) {
+         System.err.println(e);
+      }
    }
 }
