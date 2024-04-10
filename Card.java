31401a35d4c2c83b2bf3f4d914f518bbe6582075
bigMARAC <marcos.length@gmail.com>
2024-04-10 20:03:52 -0300

    cards-poker

--- Card.java
+++ Card.java
@@ -2,10 +2,36 @@ public class Card {
    private final String face;
    private final String suit;
 
+   public String getFace() {
+      return face;
+   }
+   
+   public static String[] getFaces(Card[] hand) {
+      String[] allFaces = new String[hand.length];
+      for (int i = 0; i < allFaces.length; i++) {
+         allFaces[i] = hand[i].getFace();
+      }
+      
+      return allFaces;
+   }
+
+   public String getSuit() {
+      return suit;
+   }
+   
+   public static String[] getSuits(Card[] hand) {
+      String[] allFaces = new String[hand.length];
+      for (int i = 0; i < allFaces.length; i++) {
+         allFaces[i] = hand[i].getSuit();
+      }
+      
+      return allFaces;
+   }
+
    public Card(String face, String suit) {
       this.face = face;
       this.suit = suit; 
-   } 
+   }
 
    public String toString() { 
       return face + " de " + suit;
--- DeckOfCards.java
+++ DeckOfCards.java
@@ -1,50 +1,126 @@
 import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 
 public class DeckOfCards {
-   private Card[] deck; // array of Card objects
-   private int currentCard; // index of next Card to be dealt (0-51)
-   private static final int NUMBER_OF_CARDS = 52; // constant # of Cards
-   // random number generator
+   private Card[] deck; 
+   private int currentCard;
+   private static final int NUMBER_OF_CARDS = 52;
    private static final SecureRandom randomNumbers = new SecureRandom();
+   private final String[] faces = {"As", "Dois", "Três", "Quatro", "Cinco", "Seis", 
+      "Sete", "Oito", "Nove", "Dez", "Valete", "Rainha", "Rei"};
+   private final String[] suits = {"Copas", "Ouro", "Paus", "Espadas"};
 
-   // constructor fills deck of Cards
    public DeckOfCards() {
-      String[] faces = {"As", "Dois", "Três", "Quatro", "Cinco", "Seis", 
-         "Sete", "Oito", "Nove", "Dez", "Valete", "Rainha", "Rei"};
-      String[] suits = {"Copas", "Ouro", "Paus", "Espadas"};
+      deck = new Card[NUMBER_OF_CARDS];
+      currentCard = 0;
 
-      deck = new Card[NUMBER_OF_CARDS]; // create array of Card objects
-      currentCard = 0; // first Card dealt will be deck[0]
-
-      // populate deck with Card objects
       for (int count = 0; count < deck.length; count++) 
          deck[count] = 
             new Card(faces[count % 13], suits[count / 13]);
    } 
 
-   // shuffle deck of Cards with one-pass algorithm
    public void shuffle() {
-      // next call to method dealCard should start at deck[0] again
       currentCard = 0; 
 
-      // for each Card, pick another random Card (0-51) and swap them
       for (int first = 0; first < deck.length; first++) {
-         // select a random number between 0 and 51 
          int second =  randomNumbers.nextInt(NUMBER_OF_CARDS);
          
-         // swap current Card with randomly selected Card
          Card temp = deck[first];        
          deck[first] = deck[second];   
          deck[second] = temp;            
       } 
    } 
 
-   // deal one Card
-   public Card dealCard() {
-      // determine whether Cards remain to be dealt
-      if (currentCard < deck.length)
-         return deck[currentCard++]; // return current Card in array
-      else        
-         return null; // return null to indicate that all Cards were dealt
-   } 
-} // end class DeckOfCards
+   public Card[][] dealCard(int hands) {
+      Card[][] allHands = new Card[hands][NUMBER_OF_CARDS / hands];
+      
+      for (int i = 0; i < allHands.length; i++) {
+         for (int j = 0; j < allHands[i].length; j++) {
+            allHands[i][j] = currentCard < deck.length ? deck[currentCard++] : null;
+         }
+      }
+      
+      return allHands;
+   }
+   
+   private Map<String, Integer> countRepetitions(String [] element) {
+      Map<String, Integer> aux = new HashMap<String, Integer>();
+      
+      for (int i = 0; i < element.length; i++) {
+         if (aux.get(element[i]) != null) {
+            aux.put(element[i], aux.get(element[i]) + 1);
+         } else {
+            aux.put(element[i], 1);
+         }
+      }
+      
+      return aux;
+   }
+   
+   public String getCombinations(Card[] hand) {
+      String[] faces = Card.getFaces(hand);
+      String[] suits = Card.getSuits(hand);
+      Map<String, Integer> suitsAux = this.countRepetitions(suits);
+      Map<String, Integer> facesAux = this.countRepetitions(faces);
+      Map<String, Integer> suitsRealValues = new HashMap<String, Integer>();
+      
+      for (int i = 0; i < this.faces.length; i++) {
+         suitsRealValues.put(this.faces[i], i + 1);
+      }
+      
+      int[] values = new int[5];
+      for (int i = 0; i < values.length; i++) {
+         for (int j = 0; j < faces.length; j++) {
+            values[j] = suitsRealValues.get(faces[j]);
+         }
+      }
+      Arrays.sort(values);
+      
+      int counter = 0;
+      if (facesAux.size() == 5) {
+         for (int i = 0; i < values.length; i++) {
+            if (i == 0) {
+               if (values[i] == 1 && (values[i+1] == 2) && (values[i+2]) == 3) counter++;
+               continue;
+            }
+            
+            if (i == values.length - 1) {
+               if (values[i] == 1 && (values[i-1] == 13) && (values[i-2]) == 12) counter++;
+               continue;
+            }
+            
+            if ((values[i] - 1 == values[i-1]) && (values[i] + 1 == values[i+1])) counter++;
+         }
+         
+         if (counter >= 3) return "STRAIGHT";
+      }
+      
+      // full house, quadra
+      if (facesAux.size() == 2) {
+         for (Map.Entry<String, Integer> entry : facesAux.entrySet()) {
+            if (entry.getValue() == 4) return "QUADRA";
+         }
+         
+         return "FULL HOUSE";
+      }
+      
+      if (suitsAux.size() == 1) return "FLUSH";
+      
+      // trinca ou dois pares
+      if (facesAux.size() == 3) {
+         for (Map.Entry<String, Integer> entry : facesAux.entrySet()) {
+            if (entry.getValue() == 3) return "TRINCA";
+         }
+         
+         return "DOIS PARES";
+      }
+      
+      if (facesAux.size() == 4) return "UM PAR";
+      
+       
+      return "MÃO NORMAL";
+   }
+}
